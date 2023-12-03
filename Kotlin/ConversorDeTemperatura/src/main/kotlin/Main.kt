e5c18f4599341472b7dc3c62bf12a671a2da3db5
João Gabriel Gomes <j.gabriel.gomes2976@gmail.com>
2023-12-03 15:52:22 -0400

    estrutura do/while adicionada
    estrutura de loop para caso o usuário digitar um valor fora dos condicionais

--- Kotlin/ConversorDeTemperatura/src/main/kotlin/Main.kt
+++ Kotlin/ConversorDeTemperatura/src/main/kotlin/Main.kt
@@ -4,41 +4,57 @@ fun main() {
     println("==============================")
     println("Primeiro diga a escala em que a temperatura está!")
     print("Para Celsius use [C], Para Fahrenheit use [F], Para Kelvin use [K]: ")
-    val entrada = readln() // ler a entrada do usuário em String
-    val escalaI: Char = entrada[0] // atribui valor à variável Char passando o caractere na posição 0 da String
+    var entrada = readln() // ler a entrada do usuário em String
+    var escalaI: Char = entrada[0] // atribui valor à variável Char passando o caractere na posição 0 da String
 
-    if (escalaI == 'C') {
-        print("Agora digite a temperatura: ")
-        val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
-        val kelvin = temperatura + 273.15
-        val fahrenheitUn = (temperatura * 1.8) + 32
-        val fahrenheit = String.format("%.1f",fahrenheitUn)  /* transforma a val fahrenheitUn em String(val fahrenheit)
-         e mostra apenas o 2º numero depois da virgula de val fahrenheitUn */
-        println("Sua temperatura em fahrenheit é: $fahrenheit ºF")
-        println("Sua temperatura em kelvin é: $kelvin K")
+    var condicional: Int? = null
+    do {
 
-    }else if (escalaI == 'F') {
-        print("Agora digite a temperatura: ")
-        val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
-        val celsiusUn = (temperatura - 32) / 1.8
-        val kelvinUn = celsiusUn + 273.15
-        val celsius = String.format("%.1f",celsiusUn)  /* transforma a val celsiusUn em String(val celsius)
-         e mostra apenas o 2º numero depois da virgula de val celsiusUn */
-        val kelvin = String.format("%.2f", kelvinUn) /* transforma a val kelvinUn em String(val kelvin)
-         e mostra apenas o 2º numero depois da virgula de val kelvinUn */
-        println("Sua temperatura em celsius é: $celsius ºC")
-        println("Sua temperatura em kelvin é: $kelvin K")
+        when (escalaI) {
+            'C' -> {
+                print("Agora digite a temperatura: ")
+                val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
+                val kelvin = temperatura + 273.15
+                val fahrenheitUn = (temperatura * 1.8) + 32
+                val fahrenheit = String.format("%.1f", fahrenheitUn)  /* transforma a val fahrenheitUn em String(val fahrenheit)
+             e mostra apenas o 2º numero depois da virgula de val fahrenheitUn */
+                println("Sua temperatura em fahrenheit é: $fahrenheit ºF")
+                println("Sua temperatura em kelvin é: $kelvin K")
+                condicional = 1
 
-    }else if (escalaI == 'K') {
-        print("Agora digite a temperatura: ")
-        val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
-        val celsiusUn = temperatura - 273.15
-        val fahrenheitUn = (celsiusUn * 1.8) + 32
-        val celsius = String.format("%.1f",celsiusUn)  /* transforma a val celsiusUn em String(val celsius)
-         e mostra apenas o 2º numero depois da virgula de val celsiusUn */
-        val fahrenheit = String.format("%.2f", fahrenheitUn) /* transforma a val fahrenheitUn em String(val fahrenheit)
-         e mostra apenas o 2º numero depois da virgula de val fahrenheitUn */
-        println("Sua temperatura em celsius é: $celsius ºC")
-        println("Sua temperatura em fahrenheit é: $fahrenheit ºF")
-    }
+            }
+            'F' -> {
+                print("Agora digite a temperatura: ")
+                val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
+                val celsiusUn = (temperatura - 32) / 1.8
+                val kelvinUn = celsiusUn + 273.15
+                val celsius = String.format("%.1f", celsiusUn)  /* transforma a val celsiusUn em String(val celsius)
+             e mostra apenas o 2º numero depois da virgula de val celsiusUn */
+                val kelvin = String.format("%.2f", kelvinUn) /* transforma a val kelvinUn em String(val kelvin)
+             e mostra apenas o 2º numero depois da virgula de val kelvinUn */
+                println("Sua temperatura em celsius é: $celsius ºC")
+                println("Sua temperatura em kelvin é: $kelvin K")
+                condicional = 1
+
+            }
+            'K' -> {
+                print("Agora digite a temperatura: ")
+                val temperatura = readln().toDouble() // ler a entrada do usuário em String e converte pra Double
+                val celsiusUn = temperatura - 273.15
+                val fahrenheitUn = (celsiusUn * 1.8) + 32
+                val celsius = String.format("%.1f", celsiusUn)  /* transforma a val celsiusUn em String(val celsius)
+             e mostra apenas o 2º numero depois da virgula de val celsiusUn */
+                val fahrenheit = String.format("%.2f", fahrenheitUn) /* transforma a val fahrenheitUn em String(val fahrenheit)
+             e mostra apenas o 2º numero depois da virgula de val fahrenheitUn */
+                println("Sua temperatura em celsius é: $celsius ºC")
+                println("Sua temperatura em fahrenheit é: $fahrenheit ºF")
+                condicional = 1
+            }
+            else -> {
+                println("Erro! Adicione uma Escala Válida [C] [F] [K]: ")
+                 entrada = readln() // ler a entrada do usuário em String
+                 escalaI = entrada[0] // atribui valor à variável Char passando o caractere na posição 0 da String
+            }
+        }
+    } while (condicional == null)
 }
