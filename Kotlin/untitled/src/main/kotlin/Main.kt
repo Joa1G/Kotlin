8758986dd7cd5b07d72d6b497d57d3c776002f38
João Gabriel Gomes <j.gabriel.gomes2976@gmail.com>
2023-12-04 10:20:39 -0400

    name change

--- Kotlin/untitled/src/main/kotlin/Main.kt
+++ Kotlin/untitled/src/main/kotlin/Main.kt
@@ -2,13 +2,13 @@
 fun main() {
     do {
         print("Digite um número e verifique se ele é par ou impar: ")
-        var num: Double = readln().toDouble()
+        val num: Double = readln().toDouble()
         if (num % 2 == 0.toDouble()) {
             println("Seu número é PAR")
         } else {
             println("Seu número é IMPAR")
         }
         print("Deseja continuar?[S/N]: ")
-        var resp = readln()
+        val resp = readln()
     } while (resp == "S")
 }
